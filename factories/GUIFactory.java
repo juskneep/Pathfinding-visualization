d102bef60884c96cfe898cd975ab9d19f02bedfd
Vladislav Zinoviev <juskneep@gmail.com>
2020-04-20 09:58:52 +0300

    Fixed the problem with shaded boxes

--- factories/GUIFactory.java
+++ factories/GUIFactory.java
@@ -4,6 +4,7 @@ import java.lang.reflect.Field;
 
 import javax.swing.JComponent;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 import buttons.Button;
 import buttons.CheckBox;
@@ -19,18 +20,13 @@ public class GUIFactory {
     Panel algorithmsPane = new Panel(ApplicationConstants.algorithmsPaneX, ApplicationConstants.algorithmsPaneY);
     Panel pathPane = new Panel(ApplicationConstants.pathPaneX, ApplicationConstants.pathPaneY);
 
-    public Button startButton = new Button("Start", ApplicationConstants.mainControlPaneX + 200,
-            ApplicationConstants.mainControlPaneY + 50);
-    public Button clearButton = new Button("Clear", ApplicationConstants.mainControlPaneX + 25,
-            ApplicationConstants.mainControlPaneY + 50);
-    // Button savePathButton = new Button("Save Path", 0, 0);
-    // Button generatePathButton = new Button("Generate Path", 0, 0);
-    public CheckBox dialognals = new CheckBox("Diagonals", ApplicationConstants.mainControlPaneX + 25,
-            ApplicationConstants.mainControlPaneY + 10);
-    Slider speedSlider = new Slider(ApplicationConstants.mainControlPaneX + 150,
-            ApplicationConstants.mainControlPaneY + 10);
-    public ComboBox<AlgorithmsEnum> availableAlgorithms = new ComboBox<AlgorithmsEnum>(AlgorithmsEnum.values(),
-            ApplicationConstants.algorithmsPaneX + 25, ApplicationConstants.algorithmsPaneY + 10);
+    public Button startButton = new Button("Start", 200, 50);
+    public Button clearButton = new Button("Clear", 25, 50);
+    Button savePathButton = new Button("Save Path", 25, 25);
+    Button generatePathButton = new Button("Generate Path", 150, 50);
+    public CheckBox dialognals = new CheckBox("Diagonals", 25, 10);
+    Slider speedSlider = new Slider(150, 10);
+    public ComboBox<AlgorithmsEnum> availableAlgorithms = new ComboBox<AlgorithmsEnum>(AlgorithmsEnum.values(), 25, 10);
 
     public GUIFactory(JFrame frame) throws IllegalArgumentException, IllegalAccessException {
         initializeButtons();
@@ -40,17 +36,17 @@ public class GUIFactory {
     public void initializeButtons() {
         mainControlPane.addComponents(startButton, clearButton, dialognals, speedSlider);
         algorithmsPane.addComponents(availableAlgorithms);
-        //pathPane.addComponents(savePathButton, generatePathButton);
+        pathPane.addComponents(savePathButton, generatePathButton);
     }
 
     public void initializeControls(JFrame frame) throws IllegalArgumentException, IllegalAccessException {
         for (Field component : this.getClass().getDeclaredFields()) {
             component.setAccessible(true);
 
-            if (JComponent.class.isAssignableFrom(component.getType())) {
-                JComponent field = (JComponent) component.get(this);
+            if (Panel.class.isAssignableFrom(component.getType())) {
+                Panel field = (Panel) component.get(this);
                 frame.add(field);
-            }            
+            }
         }
         /*
          * initializePanels(); initializeButtons(); initializeSliders();
